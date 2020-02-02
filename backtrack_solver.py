9cdae620f41ff347a684e67f70959fd38b83e480
Maxim Borisov <bedlamzd@gmail.com>
2020-02-03 00:10:43 +0300

    added if __name__ == '__main__'

--- backtrack_solver.py
+++ backtrack_solver.py
@@ -19,17 +19,18 @@ def backtrack_solve(grid, *, calls=0):
     return False, new_grid, calls  # if no value fit, then backtrack
 
 
-sudoku_grid = [[5, 0, 0, 0, 0, 0, 7, 0, 9],
-               [0, 4, 0, 9, 2, 0, 0, 6, 0],
-               [7, 6, 0, 0, 0, 0, 0, 3, 0],
-               [0, 0, 6, 0, 1, 5, 0, 0, 0],
-               [0, 8, 1, 0, 7, 0, 4, 0, 0],
-               [9, 0, 0, 6, 0, 0, 0, 0, 8],
-               [0, 0, 4, 2, 6, 0, 0, 0, 1],
-               [0, 0, 0, 7, 0, 4, 2, 0, 0],
-               [0, 9, 0, 0, 0, 1, 0, 0, 0]]
+if __name__ == '__main__':
+    sudoku_grid = [[5, 0, 0, 0, 0, 0, 7, 0, 9],
+                   [0, 4, 0, 9, 2, 0, 0, 6, 0],
+                   [7, 6, 0, 0, 0, 0, 0, 3, 0],
+                   [0, 0, 6, 0, 1, 5, 0, 0, 0],
+                   [0, 8, 1, 0, 7, 0, 4, 0, 0],
+                   [9, 0, 0, 6, 0, 0, 0, 0, 8],
+                   [0, 0, 4, 2, 6, 0, 0, 0, 1],
+                   [0, 0, 0, 7, 0, 4, 2, 0, 0],
+                   [0, 9, 0, 0, 0, 1, 0, 0, 0]]
 
-_, solved_grid, calls = backtrack_solve(sudoku_grid)
+    _, solved_grid, calls = backtrack_solver(sudoku_grid)
 
-print(f'Steps done: {calls:5}')
-print_grid(solved_grid)
+    print(f'Steps done: {calls:5}')
+    print_grid(solved_grid)
--- naive_solver.py
+++ naive_solver.py
@@ -29,16 +29,19 @@ def naive_solver(grid) -> list:
     return new_grid
 
 
-iterations = 0  # number of iterations through whole grid
+if __name__ == '__main__':
+    sudoku_grid = [[5, 0, 0, 0, 0, 0, 7, 0, 9],
+                   [0, 4, 0, 9, 2, 0, 0, 6, 0],
+                   [7, 6, 0, 0, 0, 0, 0, 3, 0],
+                   [0, 0, 6, 0, 1, 5, 0, 0, 0],
+                   [0, 8, 1, 0, 7, 0, 4, 0, 0],
+                   [9, 0, 0, 6, 0, 0, 0, 0, 8],
+                   [0, 0, 4, 2, 6, 0, 0, 0, 1],
+                   [0, 0, 0, 7, 0, 4, 2, 0, 0],
+                   [0, 9, 0, 0, 0, 1, 0, 0, 0]]
 
-while True:
-    iterations += 1
-    grid = naive_solver(sudoku_grid)
-    if grid == sudoku_grid:  # check if solver inserted any new values
-        print('Sudoku solved') if solved_check(grid) else print('Solver stuck')
-        break
-    else:
-        sudoku_grid = grid
+    solved, new_grid, iterations = naive_solver(sudoku_grid)
+    print('Sudoku solved') if solved else print('Solver stuck')
 
-print(f'Iterations done: {iterations:4d}')
-print_grid(grid)
+    print(f'Iterations done: {iterations:4d}')
+    print_grid(new_grid)
